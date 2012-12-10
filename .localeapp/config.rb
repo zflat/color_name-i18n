e4a4c17b23171407be5358ed940755eac9496c8a
William Wedler <wwedler@riseup.net>
2012-12-10 08:29:49 -0500

    including configurations and lock

---
+++ .localeapp/config.rb
@@ -0,0 +1,6 @@
+Localeapp.configure do |config|
+  config.api_key                    = 'KDWK87qcYgpVjOv478mikB3Pp660IVJpDMWhOY3iyLDiipcD1j'
+  config.translation_data_directory = 'locales'
+  config.synchronization_data_file  = '.localeapp/log.yml'
+  config.daemon_pid_file            = '.localeapp/localeapp.pid'
+end
